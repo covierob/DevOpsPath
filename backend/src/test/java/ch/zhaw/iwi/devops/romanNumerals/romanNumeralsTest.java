21416cc3e517bb5b01b0f16a54ccde27868b67de
covierob <covierob@students.zhaw.ch>
2021-03-23 10:27:03 +0100

    romanNumerals

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/romanNumeralsTest.java
@@ -0,0 +1,43 @@
+package ch.zhaw.iwi.devops.romanNumerals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class romanNumeralsTest {
+
+    @Test
+    public void testRomanNumerals1() {
+
+        romanNumerals number = new romanNumerals();
+        Assert.assertEquals(1, number.romanToDecimal("I"));
+    }
+
+    @Test
+    public void testRomanNumerals3() {
+
+        romanNumerals number = new romanNumerals();
+        Assert.assertEquals(3, number.romanToDecimal("III"));
+    }
+
+    @Test
+    public void testRomanNumerals6() {
+
+        romanNumerals number = new romanNumerals();
+        Assert.assertEquals(6, number.romanToDecimal("VI"));
+    }
+
+    @Test
+    public void testRomanNumerals18() {
+
+        romanNumerals number = new romanNumerals();
+        Assert.assertEquals(18, number.romanToDecimal("XVIII"));
+    }
+
+    @Test
+    public void testRomanNumerals623() {
+
+        romanNumerals number = new romanNumerals();
+        Assert.assertEquals(623, number.romanToDecimal("DCXXIII"));
+    }
+    
+}
