f24bb77848d30c67ba2ff6b84bb38f42d0fc285f
covierob <covierob@students.zhaw.ch>
2021-05-21 10:24:56 +0200

    tests

--- backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/romanNumeralsTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/romanNumerals/romanNumeralsTest.java
@@ -41,17 +41,17 @@ public class romanNumeralsTest {
     }
 
     @Test
-    public void testRomanNumerals623() {
-
-        romanNumerals number = new romanNumerals();
-        Assert.assertEquals(623, number.romanToDecimal("DCXXIII"));
-    }
-
-    @Test
     public void testRomanNumerals54() {
 
         romanNumerals number = new romanNumerals();
         Assert.assertEquals(54, number.romanToDecimal("LIV"));
+    }
+
+    @Test
+    public void testRomanNumerals623() {
+
+        romanNumerals number = new romanNumerals();
+        Assert.assertEquals(623, number.romanToDecimal("DCXXIII"));
     }
     
 }
